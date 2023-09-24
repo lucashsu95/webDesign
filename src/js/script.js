27f05fb60b1665c003af64426235a617ab27da30
lucashsu95 <lucashsu95@gmail.com>
2023-09-24 11:14:18 +0800

    0924_11:14

--- src/js/script.js
+++ src/js/script.js
@@ -1,63 +1,16 @@
-const app = Vue.createApp({
-  data() {
+const { ref, createApp } = Vue
+import { main_data } from './data.js';
+
+const app = createApp({
+  setup() {
+    const navToggleFlag = ref(false)
+    const main = ref(main_data)
+    
     return {
-      navToggleFlag: false,
-      animation: [
-        "animation.html",
-        "loading.html",
-        "normal.html",
-        "國際賽.html",
-        "tranform-filter/index.html",
-        'footer/footer.html',
-        'wordEffect.html',
-      ],
-      background: ["background.html"],
-      button: [
-        "button.html",
-        "button2.html",
-        "button3.html",
-        "remember.html",
-        "remember2.0/index.html",
-      ],
-      canvas: ["drawball.html", "調色盤.html", "小畫家"],
-      card: ["border-cards", "Glass-morphism", "img-card"],
-      carry_system: ["Carry_system.html"],
-      clipPath: ["index.html"],
-      chat: ['chat.html', 'chat2.html'],
-      components: [
-        '頁面/index.html',
-        '書店/index.html',
-        'contactUs聯絡我們/index.html',
-        'login登入/login.html',
-      ],
-      hover: ["hover.html"],
-      input: ["input.html"],
-      navbar: [
-        "menu-hover-part2",
-        "menu-hover",
-        "navbar-RWD",
-        "navbar-基本款",
-        "自動縮放navbar/index.html",
-        "自動縮放navbar/index2.html",
-      ],
-      parallax: ["scroll.html", "mousemove.html"],
-      QandA: ["Q&A.html"],
-      range: ["range.html"],
-      scroll: ["index.html", "opacity.html"],
-      TodoList: [
-        'index.html',
-        'index-modal.html',
-        '超簡單版/todoList.html',
-      ],
-      slider: [
-        "slider.html",
-        '火車過山洞.html',
-        "slider-responsiveImg.html",
-        "slider-fade.html",
-        "only-css.html",
-        "vue-lg.html",
-      ],
-      shopcar: ['index.html', 'index-vue.html']
-    };
-  },
-}).mount("#app");
+      navToggleFlag,
+      main
+    }
+  }
+});
+
+app.mount("#app");
